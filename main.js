d795680de408b2bf5babaeb1c400c486664afc80
David Roesler <roeslerdavidthomas@gmail.com>
2023-08-14 14:38:38 -0700

    Finished L10

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeItem(id) {
+            itemIndex = this.cart.indexOf(id)
+            if (itemIndex > -1) {
+                this.cart.splice(itemIndex, 1)
+            }
+        },
+    }
 })
